55e779e5fc8678828329af849f88b588b28f7cea
Jalavadi Naveen <jalavadin@virtusa.com>
2023-08-26 23:32:08 +0530

    updated random

--- Multiplayer Trivia Game/admin-service/src/main/java/com/hackathon/adminservice/repo/AdminRepository.java
+++ Multiplayer Trivia Game/admin-service/src/main/java/com/hackathon/adminservice/repo/AdminRepository.java
@@ -10,7 +10,7 @@ import com.hackathon.adminservice.entity.Question;
 
 public interface AdminRepository extends JpaRepository<Question,Integer> {
 	
-	@Query(value = "SELECT question_description,opt1,opt2,opt3,opt4 FROM Question " +
-			"WHERE category = :category AND level = :level limit :limit", nativeQuery = true)
+	@Query(value = "SELECT distinct question_description,opt1,opt2,opt3,opt4 FROM Question " +
+			"WHERE category = :category AND level = :level order by rand() limit :limit", nativeQuery = true)
 	public List<Question1> getListOfQuestions(String category, String level,int limit);
 }
